285692b9db90a24b5f2b5e6fbfabc502bff3a0e9
FraserKenny <FKENNY200@caledonian.ac.uk>
2019-04-07 16:29:52 +0100

    Submission

---
+++ Assets/Scripts/HostileAgent.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HostileAgent : MonoBehaviour
+{
+    [SerializeField]
+    Transform _destination;
+    NavMeshAgent _navMeshAgent;
+    public int remainingAgents;
+    public int agentsDestroyed;
+
+    int rndFindHiding;
+
+    int speed;
+
+    public int damage;
+    //int range;
+
+    bool hasBeenHit;
+
+    //find target variables
+    GameObject[] gos;
+    public GameObject closest;
+    public GameObject secClosest;
+    public GameObject notClosest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //you could do a method to spawn agents
+        //i didnt due to how i was locating the nearest agent
+
+        //use navmesh and select first target
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if (_navMeshAgent == null)
+        {
+            Debug.Log("you aint attached the navmesh dummy");
+        }
+        else
+        {
+            SetDestination();
+        }
+
+        //set values
+        speed = 5; //moves faster than base agents
+        //damage = 35; //will take 3 hits to defeat a normal agent
+        
+        hasBeenHit = false; //if a normal agent hits, then slow by 50%
+    }
+
+    private void SetDestination()
+    {
+
+        //find closest agent set that as target
+        gos = GameObject.FindGameObjectsWithTag("Target");
+        
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            float distance2;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+
+            else if (curDistance > distance)
+            {
+                notClosest = go;
+                distance2 = curDistance;
+            }
+            else
+            {
+                secClosest = go;
+            }
+        }
+
+        Vector3 targetDest = closest.transform.position;
+        _navMeshAgent.SetDestination(targetDest);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rndFindHiding = 0;
+        SetDestination();
+        NormalAgent normalAgent = closest.GetComponent<NormalAgent>();
+
+        if(normalAgent.hiding == true)
+        {
+
+            rndFindHiding = Random.Range(0, 2);
+            if (rndFindHiding >= 1)
+            {
+                Vector3 targetDest = notClosest.transform.position;
+                _navMeshAgent.SetDestination(targetDest);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NormalAgent.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NormalAgent : MonoBehaviour
+{
+    //emotional values
+    int fear;
+    int happiness;
+    int safe;
+    int helplesness;
+    int adrenaline;
+
+    //physical condition values
+    int health;
+    float speed;
+
+    public float dangerRange = 2f;
+    public float damageRange = 1f;
+
+    [SerializeField]
+    bool patrolWaiting;
+
+    [SerializeField]
+    float totalWaitTime = 3f;
+
+    [SerializeField]
+    float switchProbability = 0.2f;
+
+    [SerializeField]
+    public GameObject[] patrolPoints;
+
+    [SerializeField]
+    Transform _destination;
+
+    public GameObject escapePoint;
+
+    NavMeshAgent navMeshAgent;
+    int currentPatrolIndex;
+    bool traveling;
+    bool waiting;
+    bool patrolForward;
+    float waitTimer;
+
+    bool damaged;
+    bool seenDamage;
+    public bool hiding;
+    bool escaped;
+    bool trapped;
+    public bool escaping;
+
+    GameObject hostile;
+
+    public Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        speed = 3.5f;
+
+        fear = 0;
+        happiness = 100;
+        safe = 100;
+        helplesness = 0;
+        adrenaline = 0;
+
+        damaged = false;
+        seenDamage = false;
+        hiding = false;
+        escaped = false;
+        trapped = false;
+        escaping = false;
+
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+
+        rend = GetComponent<Renderer>();
+
+        if (navMeshAgent == null)
+        {
+            Debug.Log("you aint attached the navmesh dummy");
+        }
+        else
+        {
+            if(patrolPoints != null && patrolPoints.Length >= 2)
+            {
+                currentPatrolIndex = 0;
+                ChangePatrolPoint();
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("you no set up nodes mang");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(traveling && navMeshAgent.remainingDistance <= 0.1f)
+        {
+            traveling = false;
+
+            if (hiding == true)
+            {
+                escaping = false;
+                waiting = true;
+                waitTimer = 0.0f;
+                ChangePatrolPoint();
+            }   
+            else
+            {
+                SetDestination();
+            }
+        }
+
+        if(waiting)
+        {
+            waitTimer += Time.deltaTime;
+            if(waitTimer >= totalWaitTime)
+            {
+                waiting = false;
+
+                ChangePatrolPoint();
+            }
+
+        }
+        if (Vector3.Distance(this.transform.position, escapePoint.transform.position) <= 0.5f)
+        {
+            escaped = true;
+        }
+
+        //effect of not escaped
+        if (!escaped)
+        {
+            safe -= 1;
+            happiness--;
+        }
+
+       
+
+        //check if hostile is close
+        Danger();
+
+        //check if adrenaline went up
+        AdrenalineIncreased();
+        //decrease adrenaline over time
+        if (adrenaline > 0)
+        {
+            adrenaline--;
+        }
+
+        //check remaining health
+        CheckHp();
+
+        //check if feeling safe
+        FeelingSafe();
+
+        if(hiding)
+        {
+            rend.material.color += new Color( 0.0f, 0.0f, 1.0f);
+            safe += 40;
+        }
+        if (damaged)
+        {
+            rend.material.color += new Color(1.0f, 0.0f, 0.0f);
+        }
+
+        //set hiding and escaping
+        if (safe > 50)
+        {
+            hiding = false;
+            escaping = true;
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (patrolPoints != null & hiding == true)
+        {
+            Vector3 targetVetor = patrolPoints[currentPatrolIndex].transform.position;
+            navMeshAgent.SetDestination(targetVetor);
+            traveling = true;
+        }
+        if (patrolPoints != null & hiding == false)
+        {
+            Vector3 targetVetor = escapePoint.transform.position;
+            navMeshAgent.SetDestination(targetVetor);
+            traveling = true; 
+        }
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if(UnityEngine.Random.Range(0f, 1f) <= switchProbability)
+        {
+            patrolForward = !patrolForward;
+
+        }
+
+        if(patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+
+        }
+        else
+        {
+            if(--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Length - 1;
+            }
+        }
+    }
+
+    private void Danger()
+    {
+        hostile = GameObject.FindGameObjectWithTag("Hostile");
+
+        if(Vector3.Distance(this.transform.position, hostile.transform.position) <= dangerRange)
+        {
+            fear++;
+            safe = -1;
+
+            adrenaline += 3;
+            //Debug.Log("in danger" + this.name);
+        }
+        if (Vector3.Distance(this.transform.position, hostile.transform.position) <= dangerRange/2)
+        {
+            fear = fear * 2;
+            safe = -5;
+            happiness = happiness / 2;
+            adrenaline += 13;
+        }
+        if (Vector3.Distance(this.transform.position, hostile.transform.position) <= damageRange)
+        {
+            HostileAgent hostileAgent = hostile.GetComponent<HostileAgent>();
+            int hpLost = hostileAgent.damage;
+
+            health = health - hpLost;
+            damaged = true;
+            helplesness += helplesness + 10; 
+            adrenaline += 33;
+            safe = -40;
+            Debug.Log("damage taken" + this.name);
+
+
+        }
+        if (Vector3.Distance(this.transform.position, hostile.transform.position) <= damageRange/2)
+        {
+            trapped = true;
+        }
+
+        if (escaped == true)
+        {
+            HostileAgent hostileAgent = hostile.GetComponent<HostileAgent>();
+            hostileAgent.remainingAgents--;
+            escaped = true;
+            this.gameObject.SetActive(false);
+        }
+
+
+    }
+
+    private void AdrenalineIncreased()
+    {
+        bool msgsent;
+        if(adrenaline>33)
+        {
+            if (this.navMeshAgent.speed > speed)
+            {
+                //light adrenaline so faster
+                this.navMeshAgent.speed += this.navMeshAgent.speed / 2;
+                Debug.Log("going faster" + this.name);
+            }
+
+        }
+        if (adrenaline > 66)
+        {
+
+        }
+        if (adrenaline > 75)
+        {
+            //too high takes damage
+            //health--;
+            msgsent = true;
+            if (msgsent == false)
+            {
+                Debug.Log("adrenaline damage taken" + this.name);
+            }
+
+            //rend.material.color = Color.blue;
+        }
+    }
+
+    private void CheckHp()
+    {
+        //just does a condition if dead
+        if(health <= 0)
+        {
+            HostileAgent hostileAgent = hostile.GetComponent<HostileAgent>();
+            hostileAgent.agentsDestroyed++;
+            hostileAgent.remainingAgents--;
+            this.gameObject.SetActive(false);
+        }
+    }
+    private void FeelingSafe()
+    {
+        //if not feeling safe will try to hide
+        if (hiding == false)
+        {
+            if (safe <= 50)
+            {
+                ChangePatrolPoint();
+                hiding = true;
+
+
+            }
+        }
+       
+    }
+}
